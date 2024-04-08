4e497a9cc6425b1918ad2085ba49aba39ce81042
justsauce <dmzverev8@gmail.com>
2024-04-08 13:05:14 +0800

    Добавьте файлы проекта.

---
+++ laba4_inheritance/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace laba4_inheritance
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRefill = new Button();
+            txtInfo = new RichTextBox();
+            SuspendLayout();
+            // 
+            // btnRefill
+            // 
+            btnRefill.Location = new Point(91, 37);
+            btnRefill.Name = "btnRefill";
+            btnRefill.Size = new Size(219, 28);
+            btnRefill.TabIndex = 0;
+            btnRefill.Text = "Перезаполнить";
+            btnRefill.UseVisualStyleBackColor = true;
+            // 
+            // txtInfo
+            // 
+            txtInfo.BorderStyle = BorderStyle.None;
+            txtInfo.Location = new Point(91, 89);
+            txtInfo.Name = "txtInfo";
+            txtInfo.ReadOnly = true;
+            txtInfo.Size = new Size(219, 50);
+            txtInfo.TabIndex = 1;
+            txtInfo.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(398, 352);
+            Controls.Add(txtInfo);
+            Controls.Add(btnRefill);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnRefill;
+        private RichTextBox txtInfo;
+    }
+}
---
+++ laba4_inheritance/Transport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba4_inheritance
+{
+    public class Transport { }
+
+    // виды велосипедов
+    public enum BicycleType { mountain, urban, children};
+    public class Bicycle {
+        public double wheelRadius = 0;
+        public BicycleType type = BicycleType.mountain; // тип
+    }
+
+    // виды автомобилей
+    public enum CarsType { bus, truck, SUV, car };
+    public class Cars {
+        public double engineCapacity = 0;
+        public double numberOfDoors = 0;
+        public CarsType type = CarsType.bus;
+    }
+
+    // самолет
+    public class Plane {
+        public string typeEngene = "";
+        public double maxHeight = 0;
+    }
+    
+}
